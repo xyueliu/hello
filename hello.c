9dc96aa2101aba538d7797be7f11cad15681ca17
xyueliu <42636341+xyueliu@users.noreply.github.com>
2019-01-17 08:41:58 -0600

    added major

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Xinyue Liu
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Xinyue Liu\n
+          Computer Science\n");
+
+  return 0;
+}
